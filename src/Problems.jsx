76cda00adbc47f8fae9a449f9b1621f0ed8ef05f
balaji1610 <balajiece1610>
2022-08-26 09:16:52 +0530

    Input

---
+++ src/Problems.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+
+const Problems = () => {
+  const [userFormData, setUserFormData] = useState({
+    xvalue: " ",
+    yvalue: " ",
+    dvalue: " ",
+  });
+  let [result, setResult] = useState("");
+  const handleChangeFromData = (event) => {
+    setUserFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const submitButtonClick = () => {
+    if (
+      isNaN(userFormData.xvalue) ||
+      userFormData.xvalue < 0 ||
+      userFormData.xvalue == 0
+    ) {
+      alert("Not Validate");
+      return false;
+    } else if (
+      isNaN(userFormData.yvalue) ||
+      userFormData.yvalue < 0 ||
+      userFormData.yvalue == 0
+    ) {
+      alert("Not Validate");
+      return false;
+    } else if (
+      isNaN(userFormData.dvalue) ||
+      userFormData.dvalue < 0 ||
+      userFormData.dvalue == 0
+    ) {
+      alert("Not Validate");
+      return false;
+    }
+
+    var Jumps =
+      (userFormData.yvalue - userFormData.xvalue) / userFormData.dvalue;
+
+    let FinalJumpsResult = Math.round(Math.abs(Jumps));
+  };
+
+  return (
+    <div>
+      <h1>Find Maximum Jumphs</h1>
+      <form>
+        X:{" "}
+        <input
+          type="text"
+          name="xvalue"
+          value={userFormData.xvalue}
+          onChange={handleChangeFromData}
+        />{" "}
+        Y:
+        <input
+          type="text"
+          name="yvalue"
+          value={userFormData.yvalue}
+          onChange={handleChangeFromData}
+        />{" "}
+        D:
+        <input
+          type="text"
+          name="dvalue"
+          value={userFormData.dvalue}
+          onChange={handleChangeFromData}
+        />
+        &nbsp; &nbsp;
+        <button type="button" onClick={submitButtonClick}>
+          Submit
+        </button>
+      </form>
+      <p class="text"></p>
+    </div>
+  );
+};
+export default Problems;
